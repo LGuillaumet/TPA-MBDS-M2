1d0510d594128b377133272ba01c84240e626ca3
Steven <steven.bouche@outlook.com>
2021-12-29 03:03:50 +0100

    clean

--- analyse_de_donnees/r_jdbc_presto_example/example.R
+++ analyse_de_donnees/r_jdbc_presto_example/example.R
@@ -9,6 +9,26 @@ library("rstudioapi")
 setwd(dirname(getActiveDocumentContext()$path))
 getwd()
 
+##
+#   Possible to use R with Spark on cluster ?
+#
+# http://eric.univ-lyon2.fr/~ricco/tanagra/fichiers/fr_Tanagra_Spark_with_R.pdf
+# https://spark.apache.org/docs/latest/sparkr.html
+#
+##
+
+##
+#   CREATE PACKAGE R
+#
+# https://thinkr.fr/creer-package-r-quelques-minutes/
+# https://tinyheero.github.io/jekyll/update/2015/07/26/making-your-first-R-package.html
+#
+##
+
+##
+#   SETUP PRESTODB CONNECTOR
+#   /!\ Todo change path of presto jar get getwd()
+##
 new_presto_driver <- JDBC(classPath = "H:/Documents/Scolaire/TPA-MBDS-M2/libs/presto-jdbc-0.266.1.jar")
 
 presto_jdbc <- dbConnect(
@@ -17,6 +37,10 @@ presto_jdbc <- dbConnect(
   url = "jdbc:presto://localhost:8080",
   SSL = TRUE
 )
+
+##
+#   Example query to presto to obtain dataframe result
+##
 
 union <- dbGetQuery(presto_jdbc, "select * from (select distinct puissance, longueur, nbPortes, nbPlaces, prix from cassandra.datalake.registration) union distinct (select distinct puissance, longueur, nbPortes, nbPlaces, prix from hive.datalake.catalogue)")
 print(union)
