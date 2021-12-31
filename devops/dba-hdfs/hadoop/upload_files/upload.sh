36ed646ccc126834707b7b1050d4fcdf004b11ed
Steven <steven.bouche@outlook.com>
2021-12-31 16:45:49 +0100

    correctif LF, postegre script et carbon data

--- devops/dba-hdfs/hadoop/upload_files/upload.sh
+++ devops/dba-hdfs/hadoop/upload_files/upload.sh
@@ -3,8 +3,8 @@
 hadoop fs -mkdir -p    /user/hive/warehouse/dba.db
 hadoop fs -chmod g+w   /user/hive/warehouse/dba.db
 
-for file in /uploadfiles/files/*.csv
+for file in /upload_files/files/*.csv
 do
     echo "Push $file to hdfs"
-    hadoop fs -put $file /user/hive/warehouse/dba.db
+    hadoop fs -put -f $file /user/hive/warehouse/dba.db
 done
