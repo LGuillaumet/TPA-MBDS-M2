15c611b6490eb935edf148f34fd6f23a3abf8490
Dorian chap <dorian.chapoulie@gmail.com>
2022-01-02 17:52:37 +0100

    adding cors for api

--- d3js-api/index.js
+++ d3js-api/index.js
@@ -3,7 +3,10 @@ const express = require('express');
 const app = express();
 const { PORT } = process.env;
 const apiRoutes = require('./routes/api');
+const cors = require('cors')
 
+
+app.use(cors())
 app.use('/api/v1', apiRoutes);
 
 app.listen(PORT, () => {
