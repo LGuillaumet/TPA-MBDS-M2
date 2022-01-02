bed6f1564f8dc29711ffbcea3088480e019068c0
Steven <steven.bouche@outlook.com>
2022-01-02 18:27:12 +0100

    fixing bug

--- injestion/DataInjestion/DataInjestion/Helpers/TypesHelper.cs
+++ injestion/DataInjestion/DataInjestion/Helpers/TypesHelper.cs
@@ -9,13 +9,13 @@ namespace DataInjestion.Helpers
             { "string", ( typeof(string), default(string), new Func<object>(() => string.Empty)) },
             { "bool", (typeof(bool), null, new Func<object>(() => default(bool))) },
             { "boolean", (typeof(bool), null, new Func<object>(() => default(bool))) },
-            { "int", (typeof(int), null, new Func<object>(() => default(int))) },
-            { "int64", (typeof(Int64), null, new Func<object>(() => default(Int64))) },
-            { "int32", (typeof(int), null, new Func<object>(() => default(int))) },
-            { "decimal", (typeof(decimal), null, new Func<object>(() => default(decimal))) },
-            { "long", (typeof(long), null, new Func<object>(() => default(long))) },
-            { "double", (typeof(double), null, new Func<object>(() => default(double))) },
-            { "float", (typeof(float), null, new Func<object>(() => default(float))) },
+            { "int", (typeof(int), -1, new Func<object>(() => default(int))) },
+            { "int64", (typeof(Int64), -1L, new Func<object>(() => default(Int64))) },
+            { "int32", (typeof(int), -1, new Func<object>(() => default(int))) },
+            { "decimal", (typeof(decimal), -1D, new Func<object>(() => default(decimal))) },
+            { "long", (typeof(long), -1L, new Func<object>(() => default(long))) },
+            { "double", (typeof(double), -1D, new Func<object>(() => default(double))) },
+            { "float", (typeof(float), -1F, new Func<object>(() => default(float))) },
             { "guid", (typeof(string), null, new Func<object>(() => Guid.NewGuid().ToString())) }
         };
     }
