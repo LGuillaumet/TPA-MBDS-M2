f9fd4afbaf7964863e1b0c0b55850c96ed488d33
Dorian chap <dorian.chapoulie@gmail.com>
2022-01-02 16:44:39 +0100

    fix typo in colors route

--- d3js-api/routes/api.js
+++ d3js-api/routes/api.js
@@ -2,9 +2,8 @@ const express = require('express');
 const router = express.Router();
 const { knex } = require('../lib/knex/init');
 
-router.get('/', async (req, res) =>{
+router.get('/colors', async (req, res) =>{
     const ret = await knex('datawarehouse.cars').distinct().pluck('couleur');
-
     res.json({ couleurs: ret });
 });
  
