0a176a43d28818055e292d461500266f99c960e4
LGuillaumet <leo.guillaumet@etu.unice.fr>
2022-01-05 22:16:40 +0100

    page home d3js app

--- d3js-app/src/pages/Home.js
+++ d3js-app/src/pages/Home.js
@@ -1,21 +1,23 @@
-import { Container, Row, Col, Card, CardGroup, CardImg, CardText, CardBody, CardTitle, Button, CardSubtitle } from 'reactstrap';
+import { Container, Row, Col, Card, CardGroup, CardImg, CardText, CardBody, CardTitle, Button, CardFooter, ListGroupItem, ListGroup } from 'reactstrap';
 
 import d3Logo from '../assets/D3.png';
 import reactLogo from '../assets/ReactLogo.png';
 import rechartsLogo from '../assets/RechartsLogo.png';
+import gitHubLogo from '../assets/logoGitHub.png';
 
 
 import { NavBar } from '../components/NavBar';
 
+import './Home.scss';
 
 export const Home = () => {
 	return (
 		<>
 			<NavBar />
-			<Container>
+			<Container className="HomePage">
 				<Row className="justify-content-center">
 					<Col xs="12" sm="12" md="12" lg="12" xl="12">
-						<Card className="home-card">
+						<Card className="home-card my-3">
 							<Row className="justify-content-center">
 								<Col xs="12" sm="12" md="12" lg="12" xl="12">
 									<h1>TPA MIAGE M2</h1>
@@ -26,44 +28,83 @@ export const Home = () => {
 							<Col xs="12" sm="12" md="12" lg="12" xl="12">
 								<h3>Bienvenue sur la data visualization de notre projet</h3>
 								<p>Le groupe est composé des membres suivants :</p>
-								<ul className="list-group d-flex m-auto">
-									<li className="list-group-item">Steven Bouche</li>
-									<li className="list-group-item">Armand Prévot</li>
-									<li className="list-group-item">Camille Peres</li>
-									<li className="list-group-item">Dorian Chapoulié</li>
-									<li className="list-group-item">Léo Guillaumet</li>
-								</ul>
+								<ListGroup className="ListContributors">
+									<ListGroupItem
+										action
+										href="https://github.com/StevenBouche"
+										target="_blank"
+										tag="a"
+									>
+										Steven Bouche
+									</ListGroupItem>
+									<ListGroupItem
+										action
+										href="https://github.com/ArmandPrevot"
+										target="_blank"
+										tag="a"
+									>
+										Armand Prevot
+									</ListGroupItem>
+									<ListGroupItem
+										action
+										href="https://github.com/CamillePERESe"
+										target="_blank"
+										tag="a"
+									>
+										Camille Peres
+									</ListGroupItem>
+									<ListGroupItem
+										action
+										href="https://github.com/Dorian-Chapoulie"
+										target="_blank"
+										tag="a"
+									>
+										Dorian Chapoulié
+									</ListGroupItem>
+									<ListGroupItem
+										action
+										href="https://github.com/LGuillaumet"
+										target="_blank"
+										tag="a"
+									>
+										Léo Guillaumet
+									</ListGroupItem>
+								</ListGroup>
+
 							</Col>
 						</Row>
-						<Row className="justify-content-center">
+						<Row xs="auto" className="justify-content-center align-items-center mt-3">
+							<Col xs="auto">
+								<Button color="secondary" href="/#/dataviz" target="">
+									Aller vers les visualisations
+								</Button>
+							</Col>
+						</Row>
+						<Row className="justify-content-center mt-3">
 							<Col xs="12" sm="12" md="12" lg="12" xl="12">
 								<h4>Technologies utilisées</h4>
 								<p>Le groupe est composé des membres suivants :</p>
-								<CardGroup>
-									<Card>
+								<CardGroup className="Techs justify-content-center">
+									<Card className="">
 										<CardImg
-											alt="Card image cap"
+											alt="D3Logo"
 											src={d3Logo}
 											top
 											width="100%"
 										/>
 										<CardBody>
 											<CardTitle tag="h5">
-												Card title
+												D3.js
 											</CardTitle>
-											<CardSubtitle
-												className="mb-2 text-muted"
-												tag="h6"
-											>
-												Card subtitle
-											</CardSubtitle>
 											<CardText>
-												This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.
+												D3.js est une bibliothèque JavaScript permettant de manipuler des documents à partir de données. D3 vous aide à donner vie à vos données en utilisant HTML, SVG et CSS.
 											</CardText>
-											<Button>
-												Button
-											</Button>
 										</CardBody>
+										<CardFooter>
+											<Button color="info" href="https://d3js.org/" target="_blank">
+												Plus d'informations
+											</Button>
+										</CardFooter>
 									</Card>
 									<Card>
 										<CardImg
@@ -74,21 +115,16 @@ export const Home = () => {
 										/>
 										<CardBody>
 											<CardTitle tag="h5">
-												Card title
+												React
 											</CardTitle>
-											<CardSubtitle
-												className="mb-2 text-muted"
-												tag="h6"
-											>
-												Card subtitle
-											</CardSubtitle>
 											<CardText>
-												This card has supporting text below as a natural lead-in to additional content.
-											</CardText>
-											<Button>
-												Button
-											</Button>
+												React est une bibliothèque JavaScript libre développée par Facebook depuis 2013. Le but principal de cette bibliothèque est de faciliter la création d'application web monopage, via la création de composants dépendant d'un état et générant une page HTML à chaque changement d'état.											</CardText>
 										</CardBody>
+										<CardFooter>
+											<Button color="info">
+												Plus d'informations
+											</Button>
+										</CardFooter>
 									</Card>
 									<Card>
 										<CardImg
@@ -98,24 +134,38 @@ export const Home = () => {
 											width="100%"
 										/>
 										<CardBody>
-											<CardTitle tag="h5">
-												Card title
+											<CardTitle tag="h5" href="https://recharts.org/en-US/" target="_blank">
+												Recharts
 											</CardTitle>
-											<CardSubtitle
-												className="mb-2 text-muted"
-												tag="h6"
-											>
-												Card subtitle
-											</CardSubtitle>
+
 											<CardText>
-												This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.
-											</CardText>
-											<Button>
-												Button
-											</Button>
+												Recharts est une bibliothèque de graphiques redéfinis construite avec React et D3.
+												L'objectif principal de cette bibliothèque est d'aider à écrire des graphiques dans des applications React sans aucune difficulté.											</CardText>
+
 										</CardBody>
+										<CardFooter>
+											<Button color="info" href="https://fr.reactjs.org/" target="_blank">
+												Plus d'informations
+											</Button>
+										</CardFooter>
 									</Card>
 								</CardGroup>
+							</Col>
+						</Row>
+
+						<Row xs="auto" className="justify-content-center align-items-center mt-5">
+							<Col xs="auto">
+								<CardImg
+									alt="Card image cap"
+									src={gitHubLogo}
+									top
+									width="100%"
+								/>
+							</Col>
+							<Col xs="auto">
+								<Button color="secondary" href="https://github.com/StevenBouche/TPA-MBDS-M2" target="_blank">
+									Aller vers le repo git
+								</Button>
 							</Col>
 						</Row>
 					</Col>
